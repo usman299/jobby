04a6f0488147a49b5e22a9c399a1c405d3d45be3
usman299 <usman@codingcrust.com>
2021-11-19 09:53:13 -0800

    routes save

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('auth.login');
-// });
+
+
 
 
 Route::get('/', function () {
-    return redirect()->route('home');
+return redirect()->route('home');
 });
 
 Auth::routes();
