a23a36a056294f6e6b93cfaeba5d2a6031b5ffd2
usman299 <usman@codingcrust.com>
2021-11-24 04:04:22 -0800

    name remove

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
 // START APPSETTING
-Route::get('app/settings', 'Api\AppSettingController@getAppSetting')->name('app.settings');
+Route::get('app/settings', 'Api\AppSettingController@getAppSetting');
 // END START APPSETTING
 
 Route::group(['middleware' => 'auth:api'], function(){
@@ -29,14 +29,14 @@ Route::post('details', 'Api\UserController@details');
 
 // START ROUTES CATEGORY
 
-Route::get('categories', 'Api\CategoryController@geCategory')->name('categories');
-Route::get('/subcategories/{category_id}', 'Api\CategoryController@geSubCategory')->name('subcategories');
-Route::get('/skils/{category_id}/{subcategory_id}', 'Api\SkilsController@CategoryRelatedSkils')->name('skils');
+Route::get('categories', 'Api\CategoryController@geCategory');
+Route::get('/subcategories/{category_id}', 'Api\CategoryController@geSubCategory');
+Route::get('/skils/{category_id}/{subcategory_id}', 'Api\SkilsController@CategoryRelatedSkils');
 
 // JOBREQUEST
-Route::post('/job/request', 'Api\JobRequestController@jobRequestStore')->name('job.request');
-Route::get('/all/active/job', 'Api\JobRequestController@activeJobRequestGet')->name('all.active.job');
-Route::get('/all/close/job', 'Api\JobRequestController@closeJobRequestGet')->name('all.close.job');
+Route::post('/job/request', 'Api\JobRequestController@jobRequestStore');
+Route::get('/all/active/job', 'Api\JobRequestController@activeJobRequestGet');
+Route::get('/all/close/job', 'Api\JobRequestController@closeJobRequestGet');
 // ENDJOBREQUEST
 
 
@@ -44,16 +44,16 @@ Route::get('/all/close/job', 'Api\JobRequestController@closeJobRequestGet')->nam
 
 
 //PROPOSOL
-Route::post('/proposal', 'Api\ProposalController@proposalPostStore')->name('proposal');
-Route::get('/active/proposal', 'Api\ProposalController@allActiveProposal')->name('active.proposal');
-Route::get('/accept/proposal', 'Api\ProposalController@allAcceptProposal')->name('accept.proposal');
-Route::get('/reject/proposal', 'Api\ProposalController@allRejectProposal')->name('reject.proposal');
+Route::post('/proposal', 'Api\ProposalController@proposalPostStore');
+Route::get('/active/proposal', 'Api\ProposalController@allActiveProposal');
+Route::get('/accept/proposal', 'Api\ProposalController@allAcceptProposal');
+Route::get('/reject/proposal', 'Api\ProposalController@allRejectProposal');
 
 //END PROPOSOL
 
 // CONTRACT
 
-Route::post('/contract', 'Api\ContractController@contractPostStore')->name('contract');
+Route::post('/contract', 'Api\ContractController@contractPostStore');
 
 //END CONTRACT
 
