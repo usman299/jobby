f0d25830b177714de75912c64252d58bbb08ec35
Sonu <bilawal.jabbar.786@gmail.com>
2021-12-11 17:03:19 +0500

    bilawal

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -58,11 +58,6 @@ $user = Auth::user();
 
 <body class="bg-snow">
 
-<!-- Start em_loading -->
-<section class="em_loading" id="loaderPage">
-    <div class="spinner_flash"></div>
-</section>
-<!-- End. em_loading -->
 
 <div id="wrapper">
     <div id="content">
