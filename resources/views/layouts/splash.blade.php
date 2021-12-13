ed8ca1228c6b2a78fb00492ac83bdf2c6da42341
Sonu <bilawal.jabbar.786@gmail.com>
2021-12-13 22:22:13 +0500

    bilawal

--- resources/views/layouts/splash.blade.php
+++ resources/views/layouts/splash.blade.php
@@ -63,7 +63,11 @@
 
 
 <body class="h-100 d-flex align-items-center text-center">
-
+<!-- Start em_loading -->
+<section class="em_loading" id="loaderPage">
+    <div class="spinner_flash"></div>
+</section>
+<!-- End. em_loading -->
 @yield('content')
 
 
