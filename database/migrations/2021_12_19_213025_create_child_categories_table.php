4db89e31eca193a8df8f0ad0b1a19333764759d1
usman299 <usman@codingcrust.com>
2021-12-20 03:44:12 -0800

    child category

---
+++ database/migrations/2021_12_19_213025_create_child_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChildCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('child_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('countory_id')->nullable();
+            $table->string('img');
+            $table->string('backColor');
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreignId('subcategory_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('child_categories');
+    }
+}
