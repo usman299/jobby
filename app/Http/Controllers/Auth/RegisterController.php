eac5b713930f794f086a65f7640c2cdd3a8f3079
Sonu <bilawal.jabbar.786@gmail.com>
2021-12-20 16:39:27 +0500

    bilawal

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        dd($data);
         $questions = [];
         $answers = [];
         if (isset($data['category_id'])){
