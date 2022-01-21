cf15dd1025ec74880273ac9d2280d349caf13dfe
usman299 <usman@codingcrust.com>
2022-01-20 23:58:28 -0800

    final

--- resources/views/web/layout/showcase.blade.php
+++ resources/views/web/layout/showcase.blade.php
@@ -271,8 +271,8 @@
                     <div class="row">
                         <div class="col-6">
                     <p class="rights"><span>Copyright</span><span>&nbsp;&copy;&nbsp;</span><span
-                            class="copyright-year"></span><span>&nbsp;</span><a href="https://www.ikaedigital.com/">   Designed
-                            by Ikae Digital</a></p>
+                            class="copyright-year"></span><span>&nbsp;</span>Designed  by<a href="https://www.ikaedigital.com/">
+                            Ikae Digital</a></p>
                         </div>
                         <div class="col-6">
                             <p class="rights"><span>Propulse par la Credit Agricode</span><span>&nbsp;&copy;&nbsp; </span><span
