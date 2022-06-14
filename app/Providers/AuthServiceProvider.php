68ac7117f1362756b85b595462cfd1a0007c323c
usman299 <usman@codingcrust.com>
2022-06-14 00:47:37 -0700

    zzzK

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Providers;
-use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
