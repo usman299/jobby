99e37cf453d04e159e290a79bdc62ba3ca7041bf
usman299 <usman@codingcrust.com>
2022-08-02 09:59:41 +0500

    Api

---
+++ app/Http/Resources/Users/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Users;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_id' => $this->id,
+            'firstName' => $this->firstName??"",
+            'lastName'=> $this->lastName??"",
+            'phone'=> $this->phone??"",
+            'email'=> $this->email??"",
+            'address'=> $this->address??"",
+            'country'=> $this->country??"",
+            'postalCode'=> $this->postalCode??"",
+            'image'=> $this->image??"",
+            'category_id'=> $this->category_id??"",
+            'subcategory_id'=> $this->subcategory_id??"",
+            'role'=> $this->role,
+        ];
+    }
+}
