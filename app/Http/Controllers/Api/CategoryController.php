a34ab3ef0b9bff6af4bb0d1c35ad0dc4a3c3b212
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-08-06 18:19:47 +0500

    login api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,38 +15,13 @@ class CategoryController extends Controller
     public function geCategory()
     {
         $category = Category::all();
-        if($category->isEmpty()){
-            $success['message'] = 'Category not Found';
-            $success['success'] = false;
-            return response()->json( $success, 200);
-        }
-        else{
-             $data =   CategoryCollection::collection($category);
-             $success['success'] = true;
-             $success['data'] = $data;
-            return response()->json($success,200);
-        }
-
+        $data = CategoryCollection::collection($category);
+        return response()->json($data);
     }
-
-
-
     public function geSubCategory($category_id)
     {
-       $subcategory = SubCategory::where('category_id','=',$category_id)->get();
-        if($subcategory->isEmpty()){
-
-            $success['message'] = 'SubCategory not Found';
-            $success['success'] = false;
-            return response()->json( $success, 200);
-
-        }
-        else{
-
-            $data =   SubCategoryCollection::collection($subcategory);
-            $success['success'] = true;
-             $success['data'] = $data;
-            return response()->json($success,200);
-        }
+        $subcategory = SubCategory::where('category_id', '=', $category_id)->get();
+        $data = SubCategoryCollection::collection($subcategory);
+        return response()->json($data);
     }
 }
