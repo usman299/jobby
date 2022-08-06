60ed5db6a408fe318028615c296df559890ae3ad
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-08-06 18:14:18 +0500

    login api

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
-// START APPSETTING
-Route::get('app/settings', 'Api\AppSettingController@getAppSetting');
 
-// END START APPSETTING
+Route::get('app/settings', 'Api\AppSettingController@getAppSetting');
+Route::get('/slider/galery/{role}', 'Api\AppSettingController@sliderGalery');
+
+Route::get('categories', 'Api\CategoryController@geCategory');
+Route::get('/subcategories/{category_id}', 'Api\CategoryController@geSubCategory');
 
 Route::get('/faq', 'Api\AppSettingController@support');
 Route::get('/about', 'Api\AppSettingController@about');
@@ -31,21 +30,7 @@ Route::get('/about', 'Api\AppSettingController@about');
 Route::group(['middleware' => 'auth:api'], function(){
 
 Route::get('/get/profile', 'Api\UserController@getProfile');
-
-// START ROUTES CATEGORY
-Route::get('categories', 'Api\CategoryController@geCategory');
-Route::get('/subcategories/{category_id}', 'Api\CategoryController@geSubCategory');
-
-
-//Slider get
-Route::get('/slider/galery/{role}', 'Api\AppSettingController@sliderGalery');
-//End Slider get
 Route::get('/notfication', 'Api\AppSettingController@notifications');
-
-
-
-
-
 
 
 });
