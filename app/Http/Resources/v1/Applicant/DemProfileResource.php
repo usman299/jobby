ab77397e44ed214fa217880591858624c7404075
mughalusman <mughalusman5554@gmail.com>
2022-10-19 17:52:28 +0500

    done

--- app/Http/Resources/v1/Applicant/DemProfileResource.php
+++ app/Http/Resources/v1/Applicant/DemProfileResource.php
@@ -27,7 +27,7 @@ class DemProfileResource extends JsonResource
             'country'=> $this->countory->name??"",
             'country_id'=> $this->countory->id??0,
             'image'=> $this->image??"",
-            'gender'=> $this->gender??"",
+            'gender'=> $this->gender == 1 ? 'Male' : 'Female'??"",
             'description'=> $this->description??"",
             'member_since'=> $date->diffForHumans() ??"",
             'total_hire_jobber' => $this->totalHireJobber(),
