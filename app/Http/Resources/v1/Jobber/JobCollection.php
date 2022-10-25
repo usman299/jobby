535bd380fc5b0515236e5a87cfe4464c9e50a674
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-10-25 17:50:19 +0500

    Category API CHANGE

--- app/Http/Resources/v1/Jobber/JobCollection.php
+++ app/Http/Resources/v1/Jobber/JobCollection.php
@@ -32,7 +32,7 @@ class JobCollection extends JsonResource
             'distance' => (string)$this->distance()??"",
             'estimate_budget' => (string)$this->estimate_budget??"",
             'time_difference' => \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->diffInMinutes(\Carbon\Carbon::now()),
-            'duration' => $this->duration()??"",
+            'duration' => $this->duration??"",
             'service_date' => ucwords($this->service_date->translatedFormat('l d F'))??"",
             'views' => $this->totalViews(),
             'is_applied' => $this->isApplied(),
