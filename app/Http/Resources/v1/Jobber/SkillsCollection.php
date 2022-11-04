d02d80d970b71c325a79847ae865e4937eac7aa3
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-11-04 10:40:12 +0500

    sss

--- app/Http/Resources/v1/Jobber/SkillsCollection.php
+++ app/Http/Resources/v1/Jobber/SkillsCollection.php
@@ -15,11 +15,17 @@ class SkillsCollection extends JsonResource
      */
     public function toArray($request)
     {
+        $response_array = array();
+        foreach (explode(',', $this->skills) as $skill){
+            $response_array[] = $skill;
+        }
+        $output = implode(" ", $response_array);
+
         return [
             'id' => $this->id,
             'main_category' => (string)$this->main_category??"0",
             'sub_category' => (string)$this->sub_category??"0",
-            'skills' => explode(',', $this->skills),
+            'skills' => $output,
             'equipments' => explode(',', $this->equipments),
             'engagments' => explode(',', $this->engagments),
             'experience' => $this->experience,
