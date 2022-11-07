59022a712ac500d98056f67eb3129c1d3e8c2d40
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-11-07 14:34:25 +0500

    sss

--- app/Http/Resources/v1/Jobber/ProposalCollection.php
+++ app/Http/Resources/v1/Jobber/ProposalCollection.php
@@ -22,6 +22,7 @@ class ProposalCollection extends JsonResource
             'price' => $this->price??"",
             'created_at' => $this->created_at->diffForHumans(),
             'duration' => $this->duration??"",
+            'status' => (int)$this->status??0,
             'jobber' => new ProfileResource($this->jobber)
         ];
     }
