ac7afacdb69254c72cb9acbe44e2968350c4caed
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-11-28 11:44:31 +0500

    prof

--- resources/views/admin/contract/index.blade.php
+++ resources/views/admin/contract/index.blade.php
@@ -13,7 +13,10 @@
                                     <li class="nav-item"><a href="#activeContract" data-toggle="tab"
                                                             class="nav-link active show"><!--ActiveContract-->Contrat
                                             Actif</a>
-                                    </li
+                                    </li>
+                                    <li class="nav-item"><a href="#deliverContract" data-toggle="tab" class="nav-link">
+                                            <!--PandingContract-->Livrer le contrat</a>
+                                    </li>
                                     <li class="nav-item"><a href="#closeContract" data-toggle="tab" class="nav-link">
                                             <!--CompleteContract-->Contrat Complet</a>
                                     </li>
@@ -68,6 +71,65 @@
                                                                         <td><span class="badge light badge-warning">Livrer</span>
                                                                         </td>
                                                                     @endif
+                                                                    <?php
+                                                                    \Carbon\Carbon::setLocale('fr');
+                                                                    $date = \Carbon\Carbon::parse($row->created_at);
+                                                                    ?>
+                                                                    <td>{{$date->diffForHumans()}}</td>
+                                                                    <td>
+                                                                        <a href="{{route('contract.show', ['id' => $row->id])}}"
+                                                                           id="edit"
+                                                                           class="btn btn-primary shadow btn-xs sharp mr-1"
+                                                                           title="edit"><i class="fa fa-eye"></i></td>
+                                                                </tr>
+                                                            @endforeach
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="deliverContract" class="tab-pane fade">
+                                        <div class="col-12">
+                                            <div class="card">
+                                                <div class="card-header">
+                                                </div>
+                                                <div class="card-body">
+                                                    <div class="table-responsive">
+                                                        <table id="example3" class="display min-w850">
+                                                            <thead>
+                                                            <tr>
+                                                                <th><!-- id -->Id</th>
+                                                                <th><!-- jobrequesttitle -->Demandeur Nom</th>
+                                                                <th>Jobber Nom</th>
+                                                                <th>Taper</th>
+                                                                <th>Prix ​​de l'offre</th>
+                                                                <th>Statut</th>
+                                                                <th>Créer à</th>
+                                                                <th>Action</th>
+                                                            </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                            @foreach($deliverContract as $row)
+                                                                <tr>
+                                                                    {{--                                                                    {{$row->service->title ?? ''}}{{$row->proposal->jobrequest->title ?? ''}}--}}
+                                                                    <td>{{$row->id}}</td>
+                                                                    <td>{{$row->user->firstName}} {{$row->user->lastName}}</td>
+                                                                    <?php $jobber = \App\User::where('id', '=', $row->jober_id)->first(); ?>
+                                                                    <td>{{$row->jobber->firstName}} {{$row->jobber->lastName}}</td>
+                                                                    @if($row->proposal_id=== null)
+                                                                        <td><span class="badge light badge-success">Service</span>
+                                                                        </td>
+                                                                    @else
+                                                                        <td><span class="badge light badge-primary">Proposol</span>
+                                                                        </td>
+                                                                    @endif
+                                                                    <td style="text-align: center;">{{$row->price}}€
+                                                                    </td>
+                                                                    <td><span
+                                                                            class="badge light badge-primary">Achevée</span>
+                                                                    </td>
                                                                     <?php
                                                                     \Carbon\Carbon::setLocale('fr');
                                                                     $date = \Carbon\Carbon::parse($row->created_at);
