153f27a65e1e27a494468775d0e8439ae9bec855
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-12-05 15:13:41 +0500

    match profiles

--- app/Http/Controllers/Api/v1/CategoryController.php
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Category;
 use App\ChildCategory;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Resources\v1\Category\CategoryCollection;
 use App\Http\Resources\v1\Category\SubCategoryCollection;
-use App\Category;
 use App\SubCategory;
 
 
@@ -19,23 +18,47 @@ class CategoryController extends Controller
         $data = CategoryCollection::collection($category);
         return response()->json($data);
     }
+
     public function geSubCategory($category_id)
     {
         $subcategory = SubCategory::where('category_id', '=', $category_id)->get();
         $data = SubCategoryCollection::collection($subcategory);
         return response()->json($data);
     }
+
     public function getChildCategories($sub_category_id)
     {
         $subcategory = ChildCategory::where('subcategory_id', '=', $sub_category_id)->get();
         $data = SubCategoryCollection::collection($subcategory);
         return response()->json($data);
     }
-    public function searchCategory(){
-        $subcategory = SubCategory::select('id', 'title', 'img')->get();
-        $childcategory = ChildCategory::select('id', 'title', 'img')->get();
-        $array1 = collect($subcategory);
-        $array2 = collect($childcategory);
+
+    public function searchCategory()
+    {
+        $arr = [];
+        $subcategory = SubCategory::all();
+        foreach ($subcategory as $key => $subcate) {
+            $arr[$key] = [
+                "maincategory_id" => $subcate->category->id,
+                "subcategory_id" => $subcate->id,
+                "childcategory_id" => 0,
+                "title" => $subcate->title??"",
+                "image" => $subcate->img??"",
+            ];
+        }
+        $arr2 = [];
+        $childcategory = ChildCategory::all();
+        foreach ($childcategory as $key => $childcate) {
+            $arr2[$key] = [
+                "maincategory_id" => $subcate->category->id,
+                "subcategory_id" => $childcate->subcategory->id,
+                "childcategory_id" => $childcate->id,
+                "title" => $childcate->title??"",
+                "image" => $childcate->img??"",
+            ];
+        }
+        $array1 = collect($arr);
+        $array2 = collect($arr2);
         $data = $array1->merge($array2);
         return $data;
     }
